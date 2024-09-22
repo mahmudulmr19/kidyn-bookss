0ba2d8b6f9b0aaf41aae09a46782cf53d8370e9c
Mahmudul Hasan <mahmudul.dev7@gmail.com>
2024-09-22 23:59:18 +0600

    feat: shadcn installation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1 className="text-primary text-2xl">Kidyn Books</h1>
+    </div>
+  );
 }
